585fb48aea4bfd73f5429482974123c82db6116f
Laryssa Yumi Nacasima Barros <laryyumi@gmail.com>
2019-12-02 17:22:13 -0300

    Criado endpoit de atualização de carro por id

--- src/main/java/com/carros/api/CarrosController.java
+++ src/main/java/com/carros/api/CarrosController.java
@@ -5,6 +5,9 @@ import com.carros.domain.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +34,19 @@ public class CarrosController {
     @GetMapping("/tipo/{tipo}")
     public Iterable<Carro> getCarrosByTipo(@PathVariable("tipo") String tipo){
         return service.getCarrosByTipo(tipo);
+    }
+
+    @PostMapping
+    public String post(@RequestBody Carro carro){
+        Carro carroSalvo = service.save(carro);
+
+        return "Carro salvo com sucesso! " + carroSalvo.getId();
+    }
+
+    @PutMapping("/{id}")
+    public String put(@PathVariable("id") Long id, @RequestBody Carro carro){
+        Carro carroAtualizado = service.update(id, carro);
+        return "Carro Atualizado com sucesso! " + carroAtualizado.getId();
     }
 
 }
--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -2,6 +2,7 @@ package com.carros.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.Optional;
 
@@ -21,5 +22,31 @@ public class CarroService {
 
     public Iterable<Carro> getCarrosByTipo(String tipo) {
         return carroRepository.findByTipo(tipo);
+    }
+
+    public Carro save(Carro carro) {
+        return carroRepository.save(carro);
+    }
+
+    public Carro update(Long id, Carro carro) {
+        Assert.notNull(id,"Não foi possível atualizar o registro");
+
+        // Busca o carro no banco de dados
+        Optional<Carro> optional = carroRepository.findById(id);
+        if(optional.isPresent()) {
+            Carro db = optional.get();
+            // Copiar as propriedades
+            db.setNome(carro.getNome());
+            db.setTipo(carro.getTipo());
+            System.out.println("Carro id " + db.getId());
+
+            // Atualiza o carro
+            carroRepository.save(db);
+
+            return db;
+        } else {
+            return null;
+            //throw new RuntimeException("Não foi possível atualizar o registro");
+        }
     }
 }
