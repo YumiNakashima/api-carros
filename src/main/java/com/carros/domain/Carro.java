81762edcc48db4292d7012144d19a2ad74daa460
Laryssa Yumi Nacasima Barros <laryyumi@gmail.com>
2019-12-06 17:19:45 -0300

    Adicionado lombok ao projeto

--- src/main/java/com/carros/domain/Carro.java
+++ src/main/java/com/carros/domain/Carro.java
@@ -1,11 +1,14 @@
 package com.carros.domain;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Carro {
 
     @Id
@@ -13,36 +16,4 @@ public class Carro {
     private Long id;
     private String nome;
     private String tipo;
-
-    public Carro() { }
-
-    public Carro(Long id, String nome, String tipo) {
-        this.id = id;
-        this.nome = nome;
-        this.tipo = tipo;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
 }
